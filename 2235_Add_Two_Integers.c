e1eb3ab574d8d2c619d15e9bd2093a0c8127a13a
seasonwang0905 <109326752+seasonwang0905@users.noreply.github.com>
2024-12-08 15:40:42 +0800

    Construct 2235_Add_Two_Integers.c and Makefile

---
+++ 2235_Add_Two_Integers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int num1, int num2){
+    return num1 + num2;
+}
+
+int main(){
+    // Example 1
+    int num1 = 12, num2 = 5;
+    printf("Example 1: %d\n", sum(num1, num2));
+
+    // Example 2
+    num1 = -10, num2 = 4;
+    printf("Exmaple 2: %d\n", sum(num1, num2));
+
+    return 0;
+}
